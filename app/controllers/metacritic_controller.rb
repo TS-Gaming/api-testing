b63ade51bd82dab05d0664f3d1fd6b8d30cfd929
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-03-06 20:36:06 -0500

    Copied from Metacritic repo

---
+++ app/controllers/metacritic_controller.rb
@@ -0,0 +1,36 @@
+require 'unirest'
+class MetacriticController < ApplicationController
+  def show
+    # These code snippets use an open-source library.
+    @response = Unirest.post "https://byroredux-metacritic.p.mashape.com/find/game",
+    headers:{
+      "X-Mashape-Key" => "8jxjMfitOnmshjPtuOrBNALxx19mp1RoJOojsnkNMwm5lykf2f",
+      "Content-Type" => "application/x-www-form-urlencoded",
+      "Accept" => "application/json"
+    },
+    parameters:{
+      "platform" => 1,
+      "retry" => 4,
+      "title" => "The Elder Scrolls V: Skyrim"
+    }
+  end
+
+  def index
+    @response = Unirest.post "https://byroredux-metacritic.p.mashape.com/find/game",
+    headers:{
+      "X-Mashape-Key" => "8jxjMfitOnmshjPtuOrBNALxx19mp1RoJOojsnkNMwm5lykf2f",
+      "Content-Type" => "application/x-www-form-urlencoded",
+      "Accept" => "application/json"
+    },
+    parameters:{
+      "platform" => 1,
+      "retry" => 4,
+      "title" => "The Elder Scrolls V: Skyrim"
+    }
+  end
+
+  def self.game_title
+    self["results"]["name"]
+  end
+
+end
